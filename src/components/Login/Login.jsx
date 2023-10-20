72f1f1b5c066a9fc7600f5323fc7f0a0b0eea2bc
Codemyhobby <alhassandamilola16@gmail.com>
2023-10-20 08:18:18 +0100

    update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,91 @@
-import React from 'react';
+import React from 'react'
+import './Login.css';
+import {BsFillEnvelopeFill} from 'react-icons/bs';
+import {BiSolidLockAlt} from 'react-icons/bi';
+import {FaUser} from 'react-icons/fa'
+import {FcGoogle} from 'react-icons/fc'
+
 
 const login = () => {
   return (
-    <div>login</div>
+        <div className="contact-container">
+        <input type="checkbox" id="flip" />
+        <div className="cover">
+        <div className="front">
+            <img src="/login/frontImg.jpg" alt="" />
+            <div className="text">
+            <span className="text-1">Every new friend is a new adventure</span>
+            <span className="text-2">Let's get connected</span>
+            </div>
+        </div>
+        <div className="back">
+            <img className="backImg" src="/login/backImg.jpg" alt="" />
+            <div className="text">
+            <span className="text-1">Complete miles of journey with one step</span>
+            <span className="text-2">Let's get started</span>
+            </div>
+        </div>
+        </div>
+        <div className="forms">
+        <div className="form-content">
+            <div className="login-form">
+            <div className="contact-title">Login</div>
+            <form action="#">
+                <div className="input-boxes">
+                <div className="input-box">
+                    {/* <i className="fas fa-envelope"></i> */}
+                    <BsFillEnvelopeFill className='login-icon'/>
+                    <input type="text" placeholder="Enter your email" required />
+                </div>
+                <div className="input-box">
+                    {/* <i className="fas fa-lock"></i> */}
+                    <BiSolidLockAlt className='login-icon'/>
+                    <input type="password" placeholder="Enter your password" required />
+                </div>
+                <div className="text">
+                    <a href="#">Forgot password?</a>
+                    {/* <p className='point' onClick={handleSignIn}>google signin</p> */}
+                </div>
+                <div className="google-btn">                        
+                    <button  className='google-signin'> <FcGoogle className='g-icon'/> Google Signin</button>
+                </div>
+                <div className="button input-box">
+                    <input type="submit" value="Submit" />
+                </div>
+                <div className="text sign-up-text">
+                    Don't have an account? <label htmlFor="flip">Sign up now</label>
+                </div>
+                </div>
+            </form>
+            </div>
+            <div className="signup-form">
+            <div className="contact-title">Signup</div>
+            <form action="#">
+                <div className="input-boxes">
+                <div className="input-box">
+                    <FaUser className="login-icon"/>
+                    <input type="text" placeholder="Enter your name" required />
+                </div>
+                <div className="input-box">
+                    <BsFillEnvelopeFill className='login-icon'/>
+                    <input type="text" placeholder="Enter your email" required />
+                </div>
+                <div className="input-box">
+                    <BiSolidLockAlt className='login-icon'/>
+                    <input type="password" placeholder="Enter your password" required />
+                </div>
+                <div className="button input-box">
+                    <input type="submit" value="Submit" />
+                </div>
+                <div className="text sign-up-text">
+                    Already have an account? <label htmlFor="flip">Login now</label>
+                </div>
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
